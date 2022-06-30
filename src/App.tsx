ba9268c538bc0de461f226c23f501a68b5bf0d61
The-Briel-Deal <gabe@fordltc.net>
2022-06-30 12:42:43 -0400

    Got emotion setup properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from "@emotion/react";
+import Grid from "@mui/material/Grid";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <Grid
+      css={css`
+        background-color: red;
+      `}
+    >
+      dog
+    </Grid>
+  );
 }
 
-export default App
+export default App;
