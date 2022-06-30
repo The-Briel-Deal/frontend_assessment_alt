cb2197d07fbd8c7a4d4e3fc7c52feb2d8e77ea0a
The-Briel-Deal <gabe@fordltc.net>
2022-06-30 12:49:53 -0400

    Created title

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 // this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
 /** @jsxImportSource @emotion/react */
 import { jsx, css } from "@emotion/react";
+import styled from "@emotion/styled";
 import Grid from "@mui/material/Grid";
-
 function App() {
+  const Title = styled.h1`
+    font-family: "Roboto", sans-serif;
+  `;
   return (
-    <Grid
-      css={css`
-        background-color: red;
-      `}
-    >
-      dog
+    <Grid container direction="column" alignItems="center">
+      <Grid item xs={12}>
+        <Title>Card Search</Title>
+      </Grid>
     </Grid>
   );
 }
