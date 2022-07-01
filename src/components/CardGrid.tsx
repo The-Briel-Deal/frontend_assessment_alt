6f8be4cc6b2463f5890a5dab56f8c9b20944fbbb
The-Briel-Deal <gabe@fordltc.net>
2022-07-01 01:42:38 -0400

    Filled out cards

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -1,11 +1,35 @@
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import { Hit } from "../types/SearchEndpointResponse";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 export default function CardGrid(props: { cardData: Hit[] }) {
   return (
-    <Grid container direction="column" alignItems="center">
+    <Grid container alignItems="center" spacing={2} columns={12}>
       {props.cardData.map((data: Hit) => {
-        return <Card key={data.objectID}>{data.name}</Card>;
+        return (
+          <Grid item xs={4} md={3}>
+            <Card key={data.objectID}>
+              <CardHeader
+                title={data.name}
+                subheader={`${data.gradingCompany} Grade ${data.grade} - ${data.year} ${data.subject}`}
+              />
+              <CardMedia
+                component="img"
+                height="194"
+                image={data.images[0].url}
+                alt="Image of card"
+              />
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  ${data.lowestListingPrice}.00 USD
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        );
       })}
     </Grid>
   );
