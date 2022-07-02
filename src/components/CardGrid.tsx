5ecd0215233de7107919c1e8c383671b14edecce
The-Briel-Deal <gabe@fordltc.net>
2022-07-02 15:08:18 -0400

    Added conditional rendering for favorite

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -1,3 +1,5 @@
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsxImportSource @emotion/react */
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import { Hit } from "../types/SearchEndpointResponse";
@@ -5,27 +7,77 @@ import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import { css } from "@emotion/react";
+import { useState } from "react";
 export default function CardGrid(props: { cardData: Hit[] }) {
+  const [isFavorite, setIsFavorite] = useState(new Set() as Set<string>);
   return (
     <Grid container alignItems="center" spacing={2} columns={12}>
       {props.cardData.map((data: Hit) => {
         return (
-          <Grid item xs={4} md={3}>
-            <Card key={data.objectID}>
+          <Grid
+            item
+            xs={4}
+            md={3}
+            sm={2}
+            css={css`
+              height: 26em;
+            `}
+          >
+            <Card
+              key={data.objectID}
+              css={css`
+                height: 100%;
+              `}
+            >
               <CardHeader
-                title={data.name}
+                title={
+                  data.name.length < 30
+                    ? data.name
+                    : data.name.slice(0, 30) + "..."
+                }
                 subheader={`${data.gradingCompany} Grade ${data.grade} - ${data.year} ${data.subject}`}
+                css={css`
+                  height: 5em;
+                `}
               />
               <CardMedia
                 component="img"
-                height="194"
+                css={css`
+                  height: 15em;
+                `}
                 image={data.images[0].url}
                 alt="Image of card"
               />
               <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                  ${data.lowestListingPrice}.00 USD
-                </Typography>
+                <Grid
+                  container
+                  direction="row"
+                  spacing={2}
+                  justifyContent="space-between"
+                >
+                  <Grid item>
+                    <Typography variant="body2" color="text.secondary">
+                      ${data.lowestListingPrice}.00 USD
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    {(() => {
+                      if (isFavorite.has(data.objectID)) {
+                        return (
+                          <Typography variant="body2" color="text.secondary">
+                            ★
+                          </Typography>
+                        );
+                      }
+                      return (
+                        <Typography variant="body2" color="text.secondary">
+                          ☆
+                        </Typography>
+                      );
+                    })()}
+                  </Grid>
+                </Grid>
               </CardContent>
             </Card>
           </Grid>
